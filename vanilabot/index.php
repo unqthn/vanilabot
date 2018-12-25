1b4ff149bdbc9912abf9fb3f439a24732a85a2b7
unqthn <yong161@gmail.com>
2018-12-25 18:15:51 +0700

    Update index.php

--- vanilabot/index.php
+++ vanilabot/index.php
@@ -1,4 +1,3 @@
 <?php
 echo "hello";
-curl -X GET \-H 'Authorization: Bearer {Wg8cX84pYEcUEe5maa4P5HCb0Q5oHzmML+1f/5fehVj/6wk1yhRAOg9lQKAOqrGsA89IMA7VWS6QSy/pH5dssyb5onlRJdTTTqBMlPIb7WZoPpnGlJYQOjT4NsB5rxBjwD8Or1B+4LhBtlIs/xnHcwdB04t89/1O/w1cDnyilFU=}' \https://api.line.me/v1/oauth/verify
 ?>
