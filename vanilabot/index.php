5088ca98f05f317bb2c58be420df7fc3ee60436a
unqthn <yong161@gmail.com>
2019-01-19 16:18:30 +0700

    Update index.php

--- vanilabot/index.php
+++ vanilabot/index.php
@@ -3,6 +3,7 @@ require_once __DIR__ . '/lineBot.php';
 $bot = new linebot();
 $text = $bot->getMessageText();
 $userid = $bot->getUserId();
+$bot->pushText($userId, 'HelloTest');
 $bot->reply($userid);
 echo "Hello Vanillasky";
 ?>
