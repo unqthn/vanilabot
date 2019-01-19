63b47edba9500f02c231abee250a7960b67c49e1
unqthn <yong161@gmail.com>
2019-01-19 15:06:22 +0700

    Update index.php

--- vanilabot/index.php
+++ vanilabot/index.php
@@ -1,7 +1,16 @@
 <?php
 require_once __DIR__ . '/lineBot.php';
-$bot = new Linebot();
+$bot = new linebot();
 $text = $bot->getMessageText();
 $bot->reply($text);
 echo "Hello Vanillasky";
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getChannelAccessToken());
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getChannelSecret()]);
+$userMessage = getMessageText();
+if($userMessage == 'ผมยู'){
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('helloyou');
+$response = $bot->replyMessage(getChannelAccessToken(), $textMessageBuilder);
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+}
 ?>
