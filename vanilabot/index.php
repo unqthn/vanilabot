634f74f5c118e8a389ff6238c86868f20a9708cf
unqthn <yong161@gmail.com>
2019-01-19 15:35:01 +0700

    Update index.php

--- vanilabot/index.php
+++ vanilabot/index.php
@@ -5,12 +5,7 @@ $text = $bot->getMessageText();
 $bot->reply($text);
 echo "Hello Vanillasky";
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getChannelAccessToken());
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getChannelSecret()]);
-$userMessage = getMessageText();
-if($userMessage == 'ผมยู'){
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('helloyou');
-$response = $bot->replyMessage(getChannelAccessToken(), $textMessageBuilder);
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
-}
+$userid = $bot->getUserId();
+$text2 = $bot->getMessageText();
+$bot->reply($text2);
 ?>
