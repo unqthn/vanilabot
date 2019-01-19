8a51e5369cad3326a43bd9589de5d30cfc6521e9
unqthn <yong161@gmail.com>
2019-01-19 16:27:38 +0700

    Update index.php

--- vanilabot/index.php
+++ vanilabot/index.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . '/lineBot.php';
 $bot = new Linebot();
 $text = $bot->getMessageText();
+$bot->reply('ไงงงงงงงงวะ');
 if($text = 'HelloTest'){
 $userid = $bot->getUserId();
 $bot->pushText($userId, 'HelloTest');
