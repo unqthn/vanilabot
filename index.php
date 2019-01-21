da5cbde1bd661334f85b97a446df9956c9f338e3
unqthn <yong161@gmail.com>
2019-01-21 14:18:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,6 @@
 <?php
-require 'vendor/autoload.php';
-
-use Google\Cloud\Dialogflow\V2\EntityTypesClient;
-
-$entityTypesClient = new EntityTypesClient();
-$projectId = 'chaichan-1d327';
-$entityTypeId = 'Webhook';
-$formattedEntityTypeName = $entityTypesClient->entityTypeName($projectId, $entityTypeId);
-
-$entityType = $entityTypesClient->getEntityType($formattedEntityTypeName);
-foreach ($entityType->getEntities() as $entity) {
-    print(PHP_EOL);
-    printf('Entity value: %s' . PHP_EOL, $entity->getValue());
-    print('Synonyms: ');
-    foreach ($entity->getSynonyms() as $synonym) {
-        print($synonym . "\t");
-    }
-    print(PHP_EOL);
-}
-
+include('webhook.php');
+$args = ['projectId' => 'chaichan-1d327'];
+$wh = new Webhook($args);
+$wh->respond_simpleMessage('Man 55555');
 ?>
