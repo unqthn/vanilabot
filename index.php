df04fcc8b37cc23f3788971bda1a542680e6c8b9
unqthn <yong161@gmail.com>
2019-01-21 13:56:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,127 +1,22 @@
 <?php
-$method = $_SERVER['REQUEST_METHOD'];
+require 'vendor/autoload.php';
 
-//process only when method is POST
-if($method == "POST"){
-    $requestBody = file_get_contents('php://input');
-    $json = json_decode($requestBody);
+use Google\Cloud\Dialogflow\V2\EntityTypesClient;
 
-    $text = $json->queryResult->parameters->text;
-    //$text = $json->results->metadata->intentName;
- 
-    switch ($text) {
-        case 'Name':
-            $speech = "This question is too personal";
-            break;    
-        default:
-            $speech = "Sorry, I didnt get that.";
-            break;
+$entityTypesClient = new EntityTypesClient();
+$projectId = 'chaichan-1d327';
+$entityTypeId = 'Webhook';
+$formattedEntityTypeName = $entityTypesClient->entityTypeName($projectId, $entityTypeId);
+
+$entityType = $entityTypesClient->getEntityType($formattedEntityTypeName);
+foreach ($entityType->getEntities() as $entity) {
+    print(PHP_EOL);
+    printf('Entity value: %s' . PHP_EOL, $entity->getValue());
+    print('Synonyms: ');
+    foreach ($entity->getSynonyms() as $synonym) {
+        print($synonym . "\t");
     }
-
-
-
-    $response = new \stdClass();
-    $response->speech = $text;
-    $response->displayText = $speech;
-    $response->source = "webhook";
-    echo json_encode($response);
-
+    print(PHP_EOL);
 }
-else
-{
-    echo "Method not allowed";
-}
-/*
-function processMessage($update) {
-    if($update["result"]["action"] == "sayHello"){
-        sendMessage(array(
-            "source" => $update["result"]["source"],
-            "speech" => "Hello from webhook",
-            "displayText" => "Hello from webhook",
-            "contextOut" => array()
-        ));
-    }
-}
- 
-function sendMessage($parameters) {
-    echo json_encode($parameters);
-}
- 
-$update_response = file_get_contents("php://input");
-$update = json_decode($update_response, true);
-if (isset($update["result"]["action"])) {
-    processMessage($update);
-}
-//require_once __DIR__ . '/lineBot.php';
-//$bot = new Linebot();
-
-
-/////
-require_once __DIR__ . '/lineBot.php';
-$bot = new Linebot();
-$text = $bot->getMessageText();
-if($text = 'HelloTest'){
-$userid = $bot->getUserId();
-$bot->pushText($userId, 'HelloTest');
-$bot->reply($userid);
-$bot->reply($text);
-
-}
-/////
-use Dialogflow\WebhookClient;
-
-$agent = new WebhookClient(json_decode(file_get_contents('php://input'),true));
-
-echo "Hello Vanillasky";
-
-function processMessage($update) {
-    if($update["result"]["action"] == "sayHello"){
-        sendMessage(array(
-            "source" => $update["result"]["source"],
-            "speech" => "Hello from webhook",
-            "displayText" => "Hello from webhook",
-            "contextOut" => array()
-        ));
-    }
-}
-
-function sendMessage($parameters) {
-    echo json_encode($parameters);
-}
-
-$update_response = file_get_contents("php://input");
-$update = json_decode($update_response, true);
-if (isset($update["result"]["action"])) {
-    processMessage($update);
-}
-
-//////
-$method = $_SERVER['REQUEST_METHOD'];
-
-if($method == 'POST'){
-    $requestBody = file_get_contents('php://input');
-    $json = json_decode($requestBody);
-
-    $text = $json->results->metadata->intentName;
-    switch ($text) {
-        case 'Name':
-            $speech = "This question is too personal";
-            break;    
-        default:
-            $speech = "Sorry, I didnt get that.";
-            break;
-    }
-
-    $response = new \stdClass();
-    $response->speech = $speech;
-    $response->displayText = $speech;
-    $response->source = "webhook";
-    echo json_encode($response);
-}
-else
-{
-    echo "Method not allowed";
-}
-*/
 
 ?>
